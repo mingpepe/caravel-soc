cb2aa8777ea97e597564c0e86d537f595565641e
mingpepe <mingpepemath@gmail.com>
2023-04-01 09:36:01 +0800

    Fix path in install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo wget -O /tmp/riscv32-unknown-elf.gcc-12.1.0.tar.gz https://github.com/stnol
 sudo mkdir /opt/riscv
 sudo tar -xzf /tmp/riscv32-unknown-elf.gcc-12.1.0.tar.gz -C /opt/riscv
 git clone https://github.com/bol-edu/caravel-soc
-chmod +x caravel-soc/testbench/counter_la/run_sim ~/caravel-soc/testbench/counter_wb/run_sim ~/caravel-soc/testbench/gcd_la/run_sim
-chmod +x caravel-soc/testbench/counter_la/run_debug ~/caravel-soc/testbench/counter_wb/run_debug ~/caravel-soc/testbench/gcd_la/run_debug
-chmod +x caravel-soc/testbench/counter_la/run_clean ~/caravel-soc/testbench/counter_wb/run_clean ~/caravel-soc/testbench/gcd_la/run_clean
+chmod +x /caravel-soc/testbench/counter_la/run_sim /caravel-soc/testbench/counter_wb/run_sim /caravel-soc/testbench/gcd_la/run_sim
+chmod +x /caravel-soc/testbench/counter_la/run_debug /caravel-soc/testbench/counter_wb/run_debug /caravel-soc/testbench/gcd_la/run_debug
+chmod +x /caravel-soc/testbench/counter_la/run_clean /caravel-soc/testbench/counter_wb/run_clean /caravel-soc/testbench/gcd_la/run_clean
 echo 'export PATH=$PATH:/opt/riscv/bin' >> ~/.bashrc
 source ~/.bashrc
 
