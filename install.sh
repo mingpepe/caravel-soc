a0572f445ecbe67e2e88f5a5c0b4a6f0af56b1f1
mingpepe <mingpepemath@gmail.com>
2023-04-11 18:59:27 +0800

    Update install.sh
    1. correct path
    2. do not need to build gtkwave
    3. just install, do not ask user

--- install.sh
+++ install.sh
@@ -4,15 +4,13 @@ sudo wget -O /tmp/riscv32-unknown-elf.gcc-12.1.0.tar.gz https://github.com/stnol
 sudo mkdir /opt/riscv
 sudo tar -xzf /tmp/riscv32-unknown-elf.gcc-12.1.0.tar.gz -C /opt/riscv
 git clone https://github.com/bol-edu/caravel-soc
-chmod +x /caravel-soc/testbench/counter_la/run_sim /caravel-soc/testbench/counter_wb/run_sim /caravel-soc/testbench/gcd_la/run_sim
-chmod +x /caravel-soc/testbench/counter_la/run_debug /caravel-soc/testbench/counter_wb/run_debug /caravel-soc/testbench/gcd_la/run_debug
-chmod +x /caravel-soc/testbench/counter_la/run_clean /caravel-soc/testbench/counter_wb/run_clean /caravel-soc/testbench/gcd_la/run_clean
+chmod +x caravel-soc/testbench/counter_la/run_sim /caravel-soc/testbench/counter_wb/run_sim /caravel-soc/testbench/gcd_la/run_sim
+chmod +x caravel-soc/testbench/counter_la/run_debug /caravel-soc/testbench/counter_wb/run_debug /caravel-soc/testbench/gcd_la/run_debug
+chmod +x caravel-soc/testbench/counter_la/run_clean /caravel-soc/testbench/counter_wb/run_clean /caravel-soc/testbench/gcd_la/run_clean
 echo 'export PATH=$PATH:/opt/riscv/bin' >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt install make g++ zlib1g-dev gdb -y
-git clone https://github.com/tomverbeure/gdbwave.git
-pushd gdbwave/src && make && popd
 
 # For X11 forwarding
-sudo apt install xauth
+sudo apt install xauth -y
